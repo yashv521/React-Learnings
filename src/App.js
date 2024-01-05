daeecf4088ca823556d986ef5387d2f026025e52
Yash Verma <yashv521@gmail.com>
2024-01-05 14:46:44 +0530

    first component and props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import Header from "./components/Header"
+
 function App() {
-    const title = 'Blog Post';
-    const body = 'This is my blog post';
-    const comments = [
-        { id: 1, text: 'Comment 1' },
-        { id: 2, text: 'Comment 2' },
-        { id: 3, text: 'Comment 3' },
-    ]
-    const loading = false;
-    const showComments = true;
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>)
-
-    if (loading) return <h1>Loading...</h1>
-
 
     return (
-        <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock}
-
-        </div>
-
+        <>
+            <Header text={true} />
+            <div className="container">
+                <h1>MyApp</h1>
+            </div>
+        </>
     )
 }
 export default App

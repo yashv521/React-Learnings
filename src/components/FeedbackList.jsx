0af053083368d91e51536778b3780daa7d581a04
Yash Verma <yashv521@gmail.com>
2024-01-06 21:58:08 +0530

    Card Component and Conditional styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types'
 
 function FeedbackList({ feedback }) {
     if (!feedback || feedback.length === 0) {
@@ -10,6 +11,16 @@ function FeedbackList({ feedback }) {
                 <FeedbackItem key={item.id} item={item} />
             ))}
         </div>
+    )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
     )
 }
 

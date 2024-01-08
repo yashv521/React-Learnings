c905f9d664de51eff687208012b27bc2b77a0d70
Yash Verma <yashv521@gmail.com>
2024-01-08 17:27:24 +0530

    Add Feedback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid"
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
@@ -14,12 +15,17 @@ function App() {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        //console.log(newFeedback)
+        setFeedback([newFeedback, ...feedback])
+    }
 
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback}
                     handleDelete={deleteFeedback} />

87ee99747153f58cf88a7c25775971ac1b2af495
Yash Verma <yashv521@gmail.com>
2024-01-08 12:03:38 +0530

    Custom Button & Real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react"
 import Card from "./shared/Card"
+import Button from "./shared/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text Entered Must be atleast 10 Characters')
+        } else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
+
         setText(e.target.value)
     }
 
@@ -15,8 +29,9 @@ function FeedbackForm() {
                 {/*@todo - rating  select component*/}
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
-                    <button type="submit">Send</button>
+                    <Button type="submit" isDisabled={btnDisabled} >Send</Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     )
